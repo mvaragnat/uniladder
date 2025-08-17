6071f5b5247c245fec4c8d29dc6ff0b1952208ac
Matthieu <matthieu.varagnat@gmail.com>
2025-08-18 00:26:14 +0300

    fix csrf + test

--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -3,8 +3,18 @@
 require 'application_system_test_case'
 
 class LoginTest < ApplicationSystemTestCase
+  # Enable CSRF protection in system tests to reproduce real browser behavior
+  setup do
+    @previous_forgery_setting = ActionController::Base.allow_forgery_protection
+    ActionController::Base.allow_forgery_protection = true
+  end
+
+  teardown do
+    ActionController::Base.allow_forgery_protection = @previous_forgery_setting
+  end
+  
   test 'user can sign in' do
-    visit new_user_session_path
+    visit new_user_session_path(locale: I18n.locale)
 
     fill_in User.human_attribute_name(:email), with: users(:player_one).email
     fill_in User.human_attribute_name(:password), with: 'password'
